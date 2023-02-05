8907740409ecd1cedcbe6d168f91811fd8776224
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-02-05 12:32:04 +0800

    feat(component): loading support

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,25 @@
+import { FC, CSSProperties } from 'react';
+import { classnames } from '~/utils';
+
+import styles from './index.module.scss';
+
+interface LoadingProps {
+  className?: string;
+  style?: CSSProperties;
+}
+
+const Loading: FC<LoadingProps> = (props) => {
+  const { className, style } = props;
+
+  return (
+    <div style={style} className={classnames(styles.wrapper, className)}>
+      <span className={styles.loading}>
+        <i />
+        <i />
+        <i />
+      </span>
+    </div>
+  );
+};
+
+export default Loading;
