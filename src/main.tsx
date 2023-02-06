44206ce2946bab175935f3f0a29d2441cc7fe45a
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-02-07 00:19:06 +0800

    feat: page analysis support

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 
 import './index.scss';
 import App from './app';
+
+inject();
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
