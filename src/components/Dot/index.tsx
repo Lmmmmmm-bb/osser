01d35574a13a6e52b56bb7381747ea4aa897ac1e
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-02-09 00:02:31 +0800

    feat(component): dot support

---
+++ src/components/Dot/index.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+
+import { classnames } from '~/utils';
+
+import styles from './index.module.scss';
+
+interface DotProps {
+  online: boolean;
+}
+
+const Dot: FC<DotProps> = (props) => (
+  <div
+    className={classnames(
+      styles.dot,
+      props.online ? styles.online : styles.offline
+    )}
+  />
+);
+
+export default Dot;
